55088105b01af4ad89f93fdc6a43664f34f7b0cd
Francis Cancio <f.b.cancio@gmail.com>
2021-03-12 14:18:57 -0600

    Completed PaintView

--- app/src/main/java/com/example/mypaint/FingerPath.java
+++ app/src/main/java/com/example/mypaint/FingerPath.java
@@ -8,15 +8,15 @@ public class FingerPath {
 
     public int color;
     public boolean emboss;
-    public boolean blue;
+    public boolean blur;
     public int strokeWidth;
     public Path path;
 
     // Class constructor generated via Code menu > Generate then select properties to use
-    public FingerPath(int color, boolean emboss, boolean blue, int strokeWidth, Path path) {
+    public FingerPath(int color, boolean emboss, boolean blur, int strokeWidth, Path path) {
         this.color = color;
         this.emboss = emboss;
-        this.blue = blue;
+        this.blur = blur;
         this.strokeWidth = strokeWidth;
         this.path = path;
     }
--- app/src/main/java/com/example/mypaint/MainActivity.java
+++ app/src/main/java/com/example/mypaint/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.mypaint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PaintView paintView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        paintView = (PaintView) findViewById(R.id.paintView);
+
+        // Get device screen info through metrics
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
     }
 }
