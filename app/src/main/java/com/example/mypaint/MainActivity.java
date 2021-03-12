b042c47132dab5c417e48f637cf9acafee02bec3
Francis Cancio <f.b.cancio@gmail.com>
2021-03-12 15:04:03 -0600

    Modified activity_main.xml, MainActivity.java and added menu main.xml.

--- app/src/main/java/com/example/mypaint/MainActivity.java
+++ app/src/main/java/com/example/mypaint/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.mypaint;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +23,30 @@ public class MainActivity extends AppCompatActivity {
         // Get device screen info through metrics
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        paintView.init(metrics);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.normal:
+                paintView.normal();
+                return true;
+            case R.id.blur:
+                paintView.blur();
+                return true;
+            case R.id.clear:
+                paintView.clear();
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
